d674b389d01d5b343b77666cd880dd71a4d2c630
axpedro <pedruax@hotmail.com>
2024-04-26 12:37:09 -0300

    Corrigido problema da side-bar e header problema do ngIf no login e cadastro adicionado o header Icone e nome do médico logado

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,36 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDrawerMode} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDrawerMode } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-import {MatListModule} from '@angular/material/list';
-
-
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: 'app-side-bar', 
+  selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterLink, RouterLinkActive, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule, DashboardComponent,MatListModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    DashboardComponent,
+    MatListModule,
+  ],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrl: './side-bar.component.css',
 })
 export class SideBarComponent {
-drawerMode: MatDrawerMode = "push";
-constructor(private router : Router){
-
-}
-
-
-  // LogOut(){
-  //   this.router.navigate(['/login']); 
-  // }
-
-
-
-
+  drawerMode: MatDrawerMode = 'push';
+  constructor(private router: Router) {}
 }
