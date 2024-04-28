44817003ff14a5f827450c641a9b65fd2bacbe36
axpedro <pedruax@hotmail.com>
2024-04-28 13:53:46 -0300

    Criado Pipe para conversao de data de nascimento em anos Adicionado ao dashboard

---
+++ src/app/pipes/age.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'age',
+  standalone: true
+})
+export class AgePipe implements PipeTransform {
+
+  transform(birthDate: string): unknown {
+    
+    if (!birthDate) return null;
+    
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    
+    
+    const month = today.getMonth() - birth.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+       
+    
+    return age;
+  }
+
+}
